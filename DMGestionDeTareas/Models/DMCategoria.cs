dee5f983a602aace4b9d6722df0f5987b75e6372
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-29 08:10:30 -0500

    Creación de las clases pilares para la aplicación.

---
+++ DMGestionDeTareas/Models/DMCategoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMGestionDeTareas.Models
+{
+    public class DMCategoria
+    {
+        public int DMCategoriaId { get; set; }
+
+        [Required]
+        public string DMNombre { get; set; }
+        public string DMDescripcion { get; set; }
+        public ICollection<DMTarea> DMTareas { get; set; }
+    }
+}
---
+++ DMGestionDeTareas/Models/DMTarea.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMGestionDeTareas.Models
+{
+    public class DMTarea
+    {
+        public int DMTareaId { get; set; }
+
+        [Required]
+        public string DMTitulo { get; set; }
+        public string DMDescripcion { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DMFechaVencimiento { get; set; }
+
+        [Required]
+        public DMPrioridad DMPrioridadTarea { get; set; }
+
+        [Required]
+        public int DMCategoriaId { get; set; }
+        public DMCategoria DMCategoria { get; set; }
+    }
+
+    public enum DMPrioridad
+    {
+        Baja,
+        Media,
+        Alta,
+        MuyAlta,
+        Urgente
+    }
+}
+
