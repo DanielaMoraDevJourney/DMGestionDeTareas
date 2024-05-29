fec32c98ca55d89405108e050e598d66ddf62860
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-29 08:08:22 -0500

    Agregar archivos de proyecto.

---
+++ DMGestionDeTareas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DMGestionDeTareas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
